c318bb9d23ffb0a8df3811b9bbf324e208be103c
Hossam Hosny <HossamHosny415@gmail.com>
2025-06-24 16:23:50 +0300

    Create Unit Test for {CurrentUser , UserContext , CreateRestaurantCommandValidator } Classes

---
+++ Restaurants.ApplicationTests/Restaurants/DTOs/RestaurantProfileTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Restaurants.Application.Restaurants.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.DTOs.Tests
+{
+    public class RestaurantProfileTests
+    {
+        [Fact()]
+        public void RestaurantProfileTest()
+        {
+
+        }
+    }
+}
--- Restaurants.Infrastructure/obj/Debug/net9.0/Restaurants.Infrastructure.AssemblyInfo.cs
+++ Restaurants.Infrastructure/obj/Debug/net9.0/Restaurants.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c8aaf9551729bc52baa70d9e77dcfda878616e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04df4d1d8a261631fd2944f4925532a02ef5d72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
