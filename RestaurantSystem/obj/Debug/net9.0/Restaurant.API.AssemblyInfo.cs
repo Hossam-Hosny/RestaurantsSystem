a7db610546a8bc0c57ba5e696136cc38a5543a3d
Hossam Hosny <HossamHosny415@gmail.com>
2025-06-25 13:48:57 +0300

    Creating xUnit test For UpdateRestaurantCommandHandler Class With Tree Senarios

--- RestaurantSystem/obj/Debug/net9.0/Restaurant.API.AssemblyInfo.cs
+++ RestaurantSystem/obj/Debug/net9.0/Restaurant.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04df4d1d8a261631fd2944f4925532a02ef5d72b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d7ef52505cfeeeedb060059cba80bbf287ec87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
